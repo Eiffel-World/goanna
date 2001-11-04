76ced8042ab6ae86c1dcd4ed0105ff00dbad25b9
glennmaughan <glennmaughan@81a1ba26-b645-43cf-9f35-cce8fbadaeef>
2001-11-04 06:25:08 +0000

    Moved fast_cgi example to servlet\fast_cgi
    git-svn-id: https://svn.origo.ethz.ch/goanna/trunk/goanna@254 81a1ba26-b645-43cf-9f35-cce8fbadaeef

--- examples/fast_cgi_server/se.bat
+++
@@ -1,1 +0,0 @@
-compile -clean -case_insensitive -no_style_warning FAST_CGI make %YAESOCKETS%\C\cl.obj wsock32.lib
--- examples/fast_cgi_server/se.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-compile -clean -case_insensitive -no_style_warning FAST_CGI_SERVER make $YAESOCKETS/C/cl.o
-
